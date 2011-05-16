e4a850b0dfc98385cfb2546eb721eb93327caf2e
Fred Eisele <feisele@isis.vanderbilt.edu>
2011-05-16 17:12:25 -0500

    Sorts the tables in descending order by id (order inserted) refs #236, needs testing

--- AmmoCore/src/edu/vu/isis/ammo/core/distributor/ui/PostalTableViewer.java
+++ AmmoCore/src/edu/vu/isis/ammo/core/distributor/ui/PostalTableViewer.java
@@ -1,5 +1,6 @@
 package edu.vu.isis.ammo.core.distributor.ui;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.TextView;
 import edu.vu.isis.ammo.core.R;
@@ -9,9 +10,32 @@ public class PostalTableViewer extends DistributorTableViewer {
 
 	private TextView tvLabel;
 	
+	static protected final String[] fromItemLayout = new String[] {
+		PostalTableSchema.URI,
+		// PostalTableSchema.PROJECTION ,
+		// PostalTableSchema.SELECTION ,
+		// PostalTableSchema.ARGS ,
+		// PostalTableSchema.ORDER ,
+		// PostalTableSchema.EXPIRATION ,
+		// PostalTableSchema.CREATED_DATE ,
+		PostalTableSchema.CREATED_DATE };
+	
 	@Override 
 	public void onCreate(Bundle bun) {
 		this.uri = PostalTableSchema.CONTENT_URI;
+		
+		String[] projection = {PostalTableSchema._ID, 
+				PostalTableSchema.DISPOSITION,
+				PostalTableSchema.URI, 
+				PostalTableSchema.CREATED_DATE};
+		
+		Cursor cursor = this.managedQuery(this.uri, projection, null, null, 
+                PostalTableSchema._ID + " DESC");
+		
+		this.adapter = new DistributorTableViewAdapter(this,
+				R.layout.distributor_table_view_item, cursor, 
+				fromItemLayout, toItemLayout);
+		
 		super.onCreate(bun);
 		
 		StringBuilder sb = new StringBuilder();
--- AmmoCore/src/edu/vu/isis/ammo/core/distributor/ui/SubscribeTableViewer.java
+++ AmmoCore/src/edu/vu/isis/ammo/core/distributor/ui/SubscribeTableViewer.java
@@ -1,5 +1,6 @@
 package edu.vu.isis.ammo.core.distributor.ui;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.TextView;
 import edu.vu.isis.ammo.core.R;
@@ -8,10 +9,32 @@ import edu.vu.isis.ammo.core.provider.DistributorSchema.SubscriptionTableSchema;
 public class SubscribeTableViewer extends DistributorTableViewer {
 
 	private TextView tvLabel;
-	
+	static protected final String[] fromItemLayout = new String[] {
+		SubscriptionTableSchema.URI,
+		// SubscriptionTableSchema.PROJECTION ,
+		// SubscriptionTableSchema.SELECTION ,
+		// SubscriptionTableSchema.ARGS ,
+		// SubscriptionTableSchema.ORDER ,
+		// SubscriptionTableSchema.EXPIRATION ,
+		// SubscriptionTableSchema.CREATED_DATE ,
+		SubscriptionTableSchema.CREATED_DATE };
+
 	@Override 
 	public void onCreate(Bundle bun) {
 		this.uri = SubscriptionTableSchema.CONTENT_URI;
+		
+		String[] projection = {SubscriptionTableSchema._ID, 
+				SubscriptionTableSchema.DISPOSITION,
+				SubscriptionTableSchema.URI, 
+				SubscriptionTableSchema.CREATED_DATE};
+		
+		Cursor cursor = this.managedQuery(this.uri, projection, null, null, 
+                SubscriptionTableSchema._ID + " DESC");
+		
+		this.adapter = new DistributorTableViewAdapter(this,
+				R.layout.distributor_table_view_item, cursor, 
+				fromItemLayout, toItemLayout);
+		
 		super.onCreate(bun);
 		
 		StringBuilder sb = new StringBuilder();
