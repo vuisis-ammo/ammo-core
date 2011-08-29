417207b6a83e3ad43f00e77dd082bb06a8ab2f9a
Nilabja Roy <nilabjar@no-spam.edu>
2011-08-29 16:10:28 -0500

    Added flag for optional security

--- AmmoCore/src/edu/vu/isis/ammo/core/security/Ammo_Crypto.java
+++ AmmoCore/src/edu/vu/isis/ammo/core/security/Ammo_Crypto.java
@@ -15,162 +15,164 @@ import javax.crypto.Cipher;
 
 public class Ammo_Crypto {
 
-	public byte[] decrypt_data (String pvt_key, byte[] data, String algo, String padding)
+  public byte[] decrypt_data (String pvt_key, byte[] data, String algo, String padding)
+  {
+    try{
+
+      System.out.println("Getting the private key");
+
+      PrivateKey pvt = getPrivateKey(pvt_key);
+
+      //			Cipher cipher = Cipher.getInstance("RSA");
+      Cipher cipher = Cipher.getInstance(algo + "/None/" + padding);
+      cipher.init(Cipher.DECRYPT_MODE, pvt);
+
+      byte[] actualData = cipher.doFinal(data);
+
+      return actualData;				 
+
+    } 
+    catch (Exception e) {
+
+      System.err.println("Caught exception " + e.toString());
+    }    
+
+    return null;
+
+  }
+
+  public static PublicKey getPublicKey(String filename)
+    throws Exception 
     {
-       	try{
-       	  
-			 System.out.println("Getting the private key");
-			 
-			 PrivateKey pvt = getPrivateKey(pvt_key);
-			 
-//			Cipher cipher = Cipher.getInstance("RSA");
-			Cipher cipher = Cipher.getInstance(algo + "/None/" + padding);
-			cipher.init(Cipher.DECRYPT_MODE, pvt);
-			 
-			byte[] actualData = cipher.doFinal(data);
-			
-			return actualData;				 
-			
-       	} 
-       	catch (Exception e) {
-			    
-				System.err.println("Caught exception " + e.toString());
-       	}    
-       	
-       	return null;
-    	
+
+      File f = new File(filename);
+
+      FileInputStream fis = new FileInputStream(f);
+      DataInputStream dis = new DataInputStream(fis);
+
+      byte[] keyBytes = new byte[(int)f.length()];
+
+      dis.readFully(keyBytes);
+
+      dis.close();
+
+      X509EncodedKeySpec spec =
+        new X509EncodedKeySpec(keyBytes);
+
+      KeyFactory kf = KeyFactory.getInstance("RSA");
+
+      return kf.generatePublic(spec);
     }
-	
-	public static PublicKey getPublicKey(String filename)
-	throws Exception {
-	
-	File f = new File(filename);
-	
-	FileInputStream fis = new FileInputStream(f);
-	DataInputStream dis = new DataInputStream(fis);
-
-	byte[] keyBytes = new byte[(int)f.length()];
-
-	dis.readFully(keyBytes);
-
-	dis.close();
-
-	X509EncodedKeySpec spec =
-					new X509EncodedKeySpec(keyBytes);
-	
-	KeyFactory kf = KeyFactory.getInstance("RSA");
-
-	return kf.generatePublic(spec);
-}
 
 
 
-	public static PrivateKey getPrivateKey(String filename)
-	throws Exception {
+  public static PrivateKey getPrivateKey(String filename)
+    throws Exception 
+    {
 
-	File f = new File(filename);
-	
-	FileInputStream fis = new FileInputStream(f);
-	DataInputStream dis = new DataInputStream(fis);
-	
-	byte[] keyBytes = new byte[(int)f.length()];
-	dis.readFully(keyBytes);
-	dis.close();
+      File f = new File(filename);
 
-	PKCS8EncodedKeySpec spec =
-				new PKCS8EncodedKeySpec(keyBytes);
+      FileInputStream fis = new FileInputStream(f);
+      DataInputStream dis = new DataInputStream(fis);
 
-	KeyFactory kf = KeyFactory.getInstance("RSA");
+      byte[] keyBytes = new byte[(int)f.length()];
+      dis.readFully(keyBytes);
+      dis.close();
 
-	return kf.generatePrivate(spec);
-}
+      PKCS8EncodedKeySpec spec =
+        new PKCS8EncodedKeySpec(keyBytes);
 
-	public byte[] encrypt_data (String pub_key, byte[] data, String algo, String padding)
-{
-	try {
-        System.out.println("Getting the public key");
-        PublicKey pub = getPublicKey(pub_key);
-                   
-        Cipher cipher = Cipher.getInstance(algo + "/None/" + padding);
-        
-        cipher.init(Cipher.ENCRYPT_MODE, pub);
-        
-        byte[] cipherData = cipher.doFinal(data);
-        
-        return cipherData;
-       
-     } catch (Exception e) {
-        
-        	System.err.println("Caught exception " + e.toString());
-        
-     }
-     
-     return null;
-}
+      KeyFactory kf = KeyFactory.getInstance("RSA");
 
-	public byte[] sign (String key_file, byte[] data,int data_len, String algo)
-{
-	
-	try {
-		
-		System.out.println("Getting the key pair");
-		PrivateKey priv = getPrivateKey(key_file);
-//		PublicKey pub = getPublicKey("public_key.der");
-		/* Create a Signature object and initialize it with the private key */
- 	      
-//		Signature dsa = Signature.getInstance("SHA1withRSA", "SunJSSE");
-		Signature dsa = Signature.getInstance(algo);
-	//	Signature dsa = Signature.getInstance("SHA1withDSA", "SUN");
- 
-        dsa.initSign(priv);
-    
-        /* Update and sign the data */
-        dsa.update(data, 0, data_len);		    
-	    
-        /* Now that all the data to be signed has been read in, 
-	    generate a signature for it */
-	    byte[] realSig = dsa.sign();
-	    
-	    
-	    return realSig;
-	    
-	} 
-	catch (Exception e) {
-		
-		System.err.println("Caught exception " + e.toString());
-	}
+      return kf.generatePrivate(spec);
+    }
 
-	return null;
-}
+  public byte[] encrypt_data (String pub_key, byte[] data, String algo, String padding)
+  {
+    try {
+      System.out.println("Getting the public key");
+      PublicKey pub = getPublicKey(pub_key);
+
+      Cipher cipher = Cipher.getInstance(algo + "/None/" + padding);
+
+      cipher.init(Cipher.ENCRYPT_MODE, pub);
+
+      byte[] cipherData = cipher.doFinal(data);
+
+      return cipherData;
+
+    } catch (Exception e) {
+
+      System.err.println("Caught exception " + e.toString());
+
+    }
+
+    return null;
+  }
+
+  public byte[] sign (String key_file, byte[] data,int data_len, String algo)
+  {
+
+    try {
+
+      System.out.println("Getting the key pair");
+      PrivateKey priv = getPrivateKey(key_file);
+      //		PublicKey pub = getPublicKey("public_key.der");
+      /* Create a Signature object and initialize it with the private key */
+
+      //		Signature dsa = Signature.getInstance("SHA1withRSA", "SunJSSE");
+      Signature dsa = Signature.getInstance(algo);
+      //	Signature dsa = Signature.getInstance("SHA1withDSA", "SUN");
+
+      dsa.initSign(priv);
+
+      /* Update and sign the data */
+      dsa.update(data, 0, data_len);		    
+
+      /* Now that all the data to be signed has been read in, 
+         generate a signature for it */
+      byte[] realSig = dsa.sign();
 
 
-	public boolean verify (String pubkey, 
-							byte []sign, 
-							byte[] data, 
-							int data_len, 
-							String algo)
-	{
-		try {
-		
-			PublicKey pubKey = getPublicKey (pubkey);
-				
-			/* create a Signature object and initialize it with the public key */
-			Signature sig = Signature.getInstance(algo);
-			sig.initVerify(pubKey);
-			
-			/* Update and verify the data */
-			sig.update(data, 0, data_len);
-			
-			boolean verifies = sig.verify(sign);
-			
-			return verifies;
-		}
-		catch (Exception e) {
-		
-			System.err.println("Caught exception " + e.toString());
-		}
-		
-		return false;
-	}
+      return realSig;
+
+    } 
+    catch (Exception e) {
+
+      System.err.println("Caught exception " + e.toString());
+    }
+
+    return null;
+  }
+
+
+  public boolean verify (String pubkey, 
+      byte []sign, 
+      byte[] data, 
+      int data_len, 
+      String algo)
+  {
+    try {
+
+      PublicKey pubKey = getPublicKey (pubkey);
+
+      /* create a Signature object and initialize it with the public key */
+      Signature sig = Signature.getInstance(algo);
+      sig.initVerify(pubKey);
+
+      /* Update and verify the data */
+      sig.update(data, 0, data_len);
+
+      boolean verifies = sig.verify(sign);
+
+      return verifies;
+    }
+    catch (Exception e) {
+
+      System.err.println("Caught exception " + e.toString());
+    }
+
+    return false;
+  }
 
 }
