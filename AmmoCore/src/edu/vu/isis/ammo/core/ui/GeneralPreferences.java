374bd643e60a82a081970b1bbbb1343c65577877
Fred Eisele <phreed@gmail.com>
2012-01-03 17:03:25 -0600

    Revert "Adding a copyright notice to all source files."
    This reverts commit b37345b1ac694207215c9893cc9b4175210bc7ef.

--- AmmoCore/src/edu/vu/isis/ammo/core/ui/GeneralPreferences.java
+++ AmmoCore/src/edu/vu/isis/ammo/core/ui/GeneralPreferences.java
@@ -1,17 +1,3 @@
-/*
-Copyright(c) 2010-2012
-
-This software was developed by the Institute for Software Integrated
-Systems (ISIS) at Vanderbilt University, Tennessee, USA for the 
-Transformative Apps program under contract [contract citation, subcontract and prime contract]. 
-The United States Government has unlimited rights to this software. 
-The US government has the right to use, modify, reproduce, release, 
-perform, display, or disclose computer software or computer software 
-documentation in whole or in part, in any manner and for any 
-purpose whatsoever, and to have or authorize others to do so.
-
- */
-
 package edu.vu.isis.ammo.core.ui;
 
 import android.content.res.Resources;
