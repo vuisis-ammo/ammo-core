8e9d749bbc22680e1cde7161551a8afdee306ca0
Fred Eisele <phreed@gmail.com>
2012-01-05 09:10:33 -0600

    Added disclaimer to all ISIS java source.

--- AmmoCore/src/edu/vu/isis/ammo/core/network/INetworkService.java
+++ AmmoCore/src/edu/vu/isis/ammo/core/network/INetworkService.java
@@ -1,7 +1,13 @@
-/**
- * Defines the methods and intent actions to be used by client classes.
- * These can be done via a binder if necessary.
- */
+/*Copyright (C) 2010-2012 Institute for Software Integrated Systems (ISIS)
+This software was developed by the Institute for Software Integrated
+Systems (ISIS) at Vanderbilt University, Tennessee, USA for the 
+Transformative Apps program under DARPA, Contract # HR011-10-C-0175.
+The United States Government has unlimited rights to this software. 
+The US government has the right to use, modify, reproduce, release, 
+perform, display, or disclose computer software or computer software 
+documentation in whole or in part, in any manner and for any 
+purpose whatsoever, and to have or authorize others to do so.
+*/
 package edu.vu.isis.ammo.core.network;
 
 import java.util.List;
