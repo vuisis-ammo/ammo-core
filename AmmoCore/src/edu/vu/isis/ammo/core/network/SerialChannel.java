8dc2b5a2fa110e46476f22f11d2b6e6a14779183
Mike James <mcj@isis.vanderbilt.edu>
2012-05-10 16:43:27 -0500

    Better FIR calculation in SerialChannel.  Fix for calling sleep() with a negative duration.

--- AmmoCore/src/edu/vu/isis/ammo/core/network/SerialChannel.java
+++ AmmoCore/src/edu/vu/isis/ammo/core/network/SerialChannel.java
@@ -16,6 +16,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.Math;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Calendar;
@@ -703,7 +704,7 @@ public class SerialChannel extends NetChannel
                             // FIR Filter
                             mDelta = 0;
                             for (long d : mDeltaSamples) mDelta += d;
-                            mDelta = mDelta/numSamples;
+                            mDelta = mDelta / Math.min( mCount + 1, numSamples );
 
                             // Store samples
                             mDeltaSamples[ mCount % numSamples ] = delta;
@@ -1021,7 +1022,8 @@ public class SerialChannel extends NetChannel
                         }
                     }
                     currentGpsTime = System.currentTimeMillis() - mDelta;
-                    Thread.sleep( goalTakeTime - currentGpsTime );
+                    if ( goalTakeTime > currentGpsTime )
+                        Thread.sleep( goalTakeTime - currentGpsTime );
                 } catch ( InterruptedException ex ) {
                     logger.debug( "interrupted taking messages from send queue: {}",
                                   ex.getLocalizedMessage() );
