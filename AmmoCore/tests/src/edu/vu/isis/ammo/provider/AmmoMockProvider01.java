8ec86b42a410ac3c7911ef7f881a676f233afb33
Fred Eisele <phreed@gmail.com>
2012-08-01 18:25:00 -0500

    Used AmmoGen to create a content provider which is used to produce a mock content provider.
    This mock content provide will be used as the basis for testing of the
    request serializer as well as the content providers produced by the
    ammo code generator.

---
+++ AmmoCore/tests/src/edu/vu/isis/ammo/provider/AmmoMockProvider01.java
@@ -0,0 +1,32 @@
+package edu.vu.isis.ammo.provider;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+
+public class AmmoMockProvider01 extends AmmoMockProviderBase {
+
+  protected class AmmoMockDatabaseHelper extends AmmoMockProviderBase.AmmoMockDatabaseHelper {
+
+    protected AmmoMockDatabaseHelper(Context context) {
+      super(context, (String) null, (SQLiteDatabase.CursorFactory) null, AmmoMockSchema01.DATABASE_VERSION);
+    }
+
+  }
+
+  @Override
+  public boolean createDatabaseHelper() {
+    this.openHelper = new AmmoMockProviderBase.AmmoMockDatabaseHelper(getContext(), null, null, AmmoMockSchema01.DATABASE_VERSION) {
+
+    };
+    return true;
+  }
+ 
+  
+  protected AmmoMockProvider01( Context context ) {
+    super(context);
+  }
+  public static AmmoMockProvider01 getInstance(Context context) {
+    return new AmmoMockProvider01(context);
+  }
+}
