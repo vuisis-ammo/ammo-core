d944ff1f4069b59e5c9b65118c0ae7cad3defbb9
Fred Eisele <phreed@gmail.com>
2012-08-03 13:25:22 -0500

    Revised the payload and topic tests to check for null source.

--- AmmoCore/tests/src/edu/vu/isis/ammo/api/type/PayloadTest.java
+++ AmmoCore/tests/src/edu/vu/isis/ammo/api/type/PayloadTest.java
@@ -3,10 +3,12 @@ package edu.vu.isis.ammo.api.type;
 
 import java.util.Arrays;
 
+import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import android.content.ContentValues;
 import android.os.Parcel;
+import android.os.Parcelable;
 import android.test.AndroidTestCase;
 /**
  * Unit test for the Payload API class 
@@ -44,7 +46,7 @@ public class PayloadTest extends AndroidTestCase
      */
     protected void setUp() throws Exception
     {
-	// ...
+        // ...
     }
 
     /**
@@ -52,7 +54,7 @@ public class PayloadTest extends AndroidTestCase
      */
     protected void tearDown() throws Exception
     {
-	// ...
+        // ...
     }
 
     /**
@@ -60,94 +62,126 @@ public class PayloadTest extends AndroidTestCase
      */
     public void testConstructorWithParcel()
     {
-	//Parcel par = null;
-	//Payload p = new Payload(par);
-	//assertNotNull(p);
+        //Parcel par = null;
+        //Payload p = new Payload(par);
+        //assertNotNull(p);
     }
 
     public void testConstructorWithString()
     {
-	final String in = "foo";
-	Payload p = new Payload(in);
-	assertNotNull(p);
+        final String in = "foo";
+        Payload p = new Payload(in);
+        assertNotNull(p);
 
-	// Need some Payload public accessors to examine content
-	// e.g.
-	// assertTrue(p.getString() == in);
+        // Need some Payload public accessors to examine content
+        // e.g.
+        // assertTrue(p.getString() == in);
     }
-    
+
     public void testConstructorWithByteArray()
     {
-	byte[] ba = new byte[10];
-	Payload p = new Payload(ba);
-	assertNotNull(p);
+        byte[] ba = new byte[10];
+        Payload p = new Payload(ba);
+        assertNotNull(p);
     }
 
     public void testConstructorWithContentValues()
     {
-	ContentValues cv = new ContentValues();
-	cv.put("ammo", "great");
-	Payload p = new Payload(cv);
-	assertNotNull(p);
+        ContentValues cv = new ContentValues();
+        cv.put("ammo", "great");
+        Payload p = new Payload(cv);
+        assertNotNull(p);
     }
-    
+
+    /**
+     * Test case of passing in a null Parcel 
+     * - should throw a null pointer exception
+     */
     public void testReadFromParcel()
     {
-	// Test case of passing in a null Parcel (should return null)
-	Parcel p1 = null;
-	Payload rv1 = Payload.readFromParcel(p1);
-	assertTrue(rv1 == null);
-	
-	// Pass in a non-null Parcel (should return non-null)
-	//Parcel p2 = new Parcel(...);
-	//Payload rv2 = Payload.readFromParcel(p2);
-	//assertNotNull(rv2);
+        /**
+         * Test case of passing in a null Parcel 
+         * - should throw a null pointer exception
+         */
+        boolean success = false;
+        try {
+            final Parcel p1 = null;
+            Payload.readFromParcel(p1);
+            
+        } catch (NullPointerException ex) {
+            success = true;
+        }
+        Assert.assertTrue("passing a null reference should fail", success);
+
+        /**
+         * Pass in a non-null Parcel containing a null payload
+         * - should return non-null
+         */
+        {
+            final Payload expected = null;
+            final Parcel parcel = Parcel.obtain();
+            Payload.writeToParcel(expected, parcel, Parcelable.PARCELABLE_WRITE_RETURN_VALUE);
+            final Payload actual = Payload.readFromParcel(parcel);
+            Assert.assertNull("wrote a null but got something else back", actual);
+        }
+        /**
+         * Pass in a non-null Parcel 
+         * - should return non-null
+         */
+        {
+            final Payload expected = new Payload("an arbitrary payload");
+            final Parcel parcel = Parcel.obtain();
+            Payload.writeToParcel(expected, parcel, Parcelable.PARCELABLE_WRITE_RETURN_VALUE);
+            final Payload actual = Payload.readFromParcel(parcel);
+            Assert.assertNotNull("wrote something but got a null back", actual);
+            Assert.assertEquals("did not get back an equivalent payload", expected, actual);
+        }
     }
-    
+
     public void testWhatContent()
     {
-	// Type STR
-	Payload p1 = new Payload("foo");
-	assertTrue(p1.whatContent() == Payload.Type.STR);
-	
-	// Type BYTE
-	byte[] ba = new byte[10];
-	Payload p2 = new Payload(ba);
-	assertNotNull(p2);
-	assertTrue(p2.whatContent() == Payload.Type.BYTE);
+        // Type STR
+        Payload p1 = new Payload("foo");
+        assertTrue(p1.whatContent() == Payload.Type.STR);
 
-	// Type CV
-	ContentValues cv = new ContentValues();
-	cv.put("ammo", "great");
-	Payload p3 = new Payload(cv);
-	assertNotNull(p3);
-	assertTrue(p3.whatContent() == Payload.Type.CV);
+        // Type BYTE
+        byte[] ba = new byte[10];
+        Payload p2 = new Payload(ba);
+        assertNotNull(p2);
+        assertTrue(p2.whatContent() == Payload.Type.BYTE);
+
+        // Type CV
+        ContentValues cv = new ContentValues();
+        cv.put("ammo", "great");
+        Payload p3 = new Payload(cv);
+        assertNotNull(p3);
+        assertTrue(p3.whatContent() == Payload.Type.CV);
     }
 
     public void testAsBytes()
     {
-	// Construct a payload from byte array
-	byte[] ba = new byte[10];
-	Payload p = new Payload(ba);
-	assertNotNull(p);
-	assertTrue(p.whatContent() == Payload.Type.BYTE);
-	
-	// Make sure the returned byte array is same as original
-	assertTrue(Arrays.equals(ba, p.asBytes()));
+        // Construct a payload from byte array
+        byte[] ba = new byte[10];
+        Payload p = new Payload(ba);
+        assertNotNull(p);
+        assertTrue(p.whatContent() == Payload.Type.BYTE);
+
+        // Make sure the returned byte array is same as original
+        assertTrue(Arrays.equals(ba, p.asBytes()));
     }
 
     public void testGetCV()
     {
-	// cv to initialize with
-	ContentValues cv = new ContentValues();
-	cv.put("foo", "bar");
-	
-	// Construct a payload with the cv
-	Payload p = new Payload(cv);
-	assertNotNull(p);
-	assertTrue(p.whatContent() == Payload.Type.CV);
+        // cv to initialize with
+        ContentValues cv = new ContentValues();
+        cv.put("foo", "bar");
 
-	// Check that retrieved cv is same as original
-	assertTrue(p.getCV() == cv);
+        // Construct a payload with the cv
+        Payload p = new Payload(cv);
+        assertNotNull(p);
+        assertTrue(p.whatContent() == Payload.Type.CV);
+
+        // Check that retrieved cv is same as original
+        assertTrue(p.getCV() == cv);
     }
 }
--- AmmoCore/tests/src/edu/vu/isis/ammo/api/type/TopicTest.java
+++ AmmoCore/tests/src/edu/vu/isis/ammo/api/type/TopicTest.java
@@ -22,115 +22,115 @@ import edu.vu.isis.ammo.api.IncompleteRequest;
 
 public class TopicTest extends AndroidTestCase 
 {
-  public TopicTest() 
-  {
-  }
-
-  public TopicTest( String testName )
-  {
-  }
-
-  /**
-   * @return the suite of tests being tested
-   */
-  public static Test suite()
-  {
-    return new TestSuite( TopicTest.class );
-  }
-
-  /**
-   * Called before every test
-   */
-  protected void setUp() throws Exception
-  {
-    // ...
-  }
-
-  /**
-   * Called after every test
-   */
-  protected void tearDown() throws Exception
-  {
-    // ...
-  }
-
-  /**
-   * Test methods
-   */
-  public void testConstructorWithParcel()
-  {
-    // Test with null Parcel arg to constructor
-    try
+    public TopicTest() 
     {
-      Parcel par1 = null;
-      Topic t1 = new Topic(par1);
-      assertNotNull(t1);
-    }
-    catch (IncompleteRequest ex) 
-    {
-      // This should not have happened
-      fail("Should not have thrown IncompleteRequest in this case");
     }
 
-    // Test constructor with Parcel 
-    try
+    public TopicTest( String testName )
     {
-      Parcel par2 = null;
-      @SuppressWarnings("unused")
-      Topic t2 = new Topic(par2);
-    }
-    catch (IncompleteRequest ex) 
-    {
-      // This should not have happened
-      fail("Should not have thrown IncompleteRequest in this case");
-    }
-    catch(Throwable ex)
-    {
-      // This should also not have happened
-      fail("Caught an unexpected exception");
     }
 
-    // Test constructor with Parcel, intentionally cause exception
-    // to be thrown (IncompleteRequest)
-    try
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
     {
-      Parcel par3 = null;
-      @SuppressWarnings("unused")
-      Topic t3 = new Topic(par3);
+        return new TestSuite( TopicTest.class );
     }
-    catch (IncompleteRequest ex) 
+
+    /**
+     * Called before every test
+     */
+    protected void setUp() throws Exception
     {
-      // Got the expected exception - correct behavior
-      assertTrue(true);
+        // ...
     }
-    catch(Throwable ex)
+
+    /**
+     * Called after every test
+     */
+    protected void tearDown() throws Exception
     {
-      // Got an unexpected exception
-      fail("Caught an unexpected exception");
+        // ...
     }
-  }
 
-  public void testConstructorWithString()
-  {
-    final String in = "foo";
-    Topic t = new Topic(in);
-    assertNotNull(t);
+    /**
+     * Test methods
+     */
+    public void testConstructorWithParcel()
+    {
+        // Test with null Parcel arg to constructor
+        try
+        {
+            Parcel par1 = null;
+            Topic t1 = new Topic(par1);
+            assertNotNull(t1);
+        }
+        catch (IncompleteRequest ex) 
+        {
+            // This should not have happened
+            fail("Should not have thrown IncompleteRequest in this case");
+        }
 
-    // Need some Topic public accessors to examine content
-    // e.g.
-    // assertTrue(t.getString() == in);
-  }
+        // Test constructor with Parcel 
+        try
+        {
+            Parcel par2 = null;
+            @SuppressWarnings("unused")
+            Topic t2 = new Topic(par2);
+        }
+        catch (IncompleteRequest ex) 
+        {
+            // This should not have happened
+            fail("Should not have thrown IncompleteRequest in this case");
+        }
+        catch(Throwable ex)
+        {
+            // This should also not have happened
+            fail("Caught an unexpected exception");
+        }
 
-  public void testReadFromParcel()
-  {
-    // Test case of passing in a null Parcel (should return null)
-    Parcel p1 = null;
-    Topic rv1 = Topic.readFromParcel(p1);
-    assertTrue(rv1 == null);
+        // Test constructor with Parcel, intentionally cause exception
+        // to be thrown (IncompleteRequest)
+        try
+        {
+            Parcel par3 = null;
+            @SuppressWarnings("unused")
+            Topic t3 = new Topic(par3);
+        }
+        catch (IncompleteRequest ex) 
+        {
+            // Got the expected exception - correct behavior
+            assertTrue(true);
+        }
+        catch(Throwable ex)
+        {
+            // Got an unexpected exception
+            fail("Caught an unexpected exception");
+        }
+    }
 
-    // Pass in a non-null Parcel (should return non-null)
-    //Parcel p2 = new Parcel(...);
-    //Topic rv2 = Topic.readFromParcel(p2);
-    //assertNotNull(rv2);
-  }
+    public void testConstructorWithString()
+    {
+        final String in = "foo";
+        Topic t = new Topic(in);
+        assertNotNull(t);
+
+        // Need some Topic public accessors to examine content
+        // e.g.
+        // assertTrue(t.getString() == in);
+    }
+
+    public void testReadFromParcel()
+    {
+        // Test case of passing in a null Parcel (should return null)
+        Parcel p1 = null;
+        Topic rv1 = Topic.readFromParcel(p1);
+        assertTrue(rv1 == null);
+
+        // Pass in a non-null Parcel (should return non-null)
+        //Parcel p2 = new Parcel(...);
+        //Topic rv2 = Topic.readFromParcel(p2);
+        //assertNotNull(rv2);
+    }
 }
