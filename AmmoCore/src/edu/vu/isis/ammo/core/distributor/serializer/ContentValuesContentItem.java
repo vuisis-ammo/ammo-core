90af10313c74e972fbc791e6ef5aebfe930994e5
Jonathon Williams <jwilliams@isis.vanderbilt.edu>
2012-08-10 14:43:33 -0500

    Add (not yet implemented) content-values content item

---
+++ AmmoCore/src/edu/vu/isis/ammo/core/distributor/serializer/ContentValuesContentItem.java
@@ -0,0 +1,87 @@
+package edu.vu.isis.ammo.core.distributor.serializer;
+
+import java.util.Set;
+
+import edu.vu.isis.ammo.core.distributor.RequestSerializer.FieldType;
+
+public class ContentValuesContentItem implements IContentItem {
+
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Set<String> keySet() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public FieldType getTypeForKey(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object get(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Boolean getAsBoolean(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Byte getAsByte(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public byte[] getAsByteArray(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Double getAsDouble(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Float getAsFloat(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Integer getAsInteger(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Long getAsLong(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Short getAsShort(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getAsString(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
