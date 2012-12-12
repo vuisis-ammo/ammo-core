41be25ae71a55425c6d9c20cad5b0758ca706341
Fred Eisele <phreed@gmail.com>
2012-12-12 14:13:36 -0600

    working test for old pretrie

--- AmmoCore/src/test/java/edu/vu/isis/ammo/util/NaivePretrieTest.java
+++ AmmoCore/src/test/java/edu/vu/isis/ammo/util/NaivePretrieTest.java
@@ -4,6 +4,8 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
+import edu.vu.isis.ammo.pretrie.Pretrie;
+
 /**
  * Test the basic functionality of the pretrie. The pretrie is a tree whose keys
  * are prefixes. When queried the pretrie returns the values having the longest
@@ -18,27 +20,29 @@ public class NaivePretrieTest {
 	@Test
 	public void basicPutAndGet() {
 		final PrefixList<String> pretrie = new PrefixList<String>();
-		pretrie.insert("abced", "abcde");
-		pretrie.insert("abcd", "abcd");
+		pretrie.insert("abcde", "abcde");
+		pretrie.insert("abcdf", "abcdf");
+		pretrie.insert("abc", "abc");
 
 		Assert.assertThat("just missed (no safetynet)",
-				pretrie.longestPrefix("abc"), CoreMatchers.is("a"));
+				pretrie.longestPrefix("ab"), CoreMatchers.nullValue());
 
 		pretrie.insert("a", "a");
 		Assert.assertThat("just missed (with safetynet)",
-				pretrie.longestPrefix("abc"), CoreMatchers.is("a"));
+				pretrie.longestPrefix("ab"), CoreMatchers.is("a"));
 
 		Assert.assertThat("exact hit",
-				pretrie.longestPrefix("abcd"),
-				CoreMatchers.is("abcd"));
+				pretrie.longestPrefix("abc"),
+				CoreMatchers.is("abc"));
 
 		Assert.assertThat("exact hit over",
 				pretrie.longestPrefix("abcde"),
 				CoreMatchers.is("abcde"));
 
 		Assert.assertThat("over shoot",
-				pretrie.longestPrefix("abcdef"),
+				pretrie.longestPrefix("abcdeg"),
 				CoreMatchers.is("abcde"));
+		
 	}
 
 }
