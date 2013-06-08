6b24f29fc40865a3b81cf25b47232dd92b0460a0
Nick King <nicholas.b.king@vanderbilt.edu>
2013-06-07 20:23:40 -0500

    Completed the State enum

--- AmmoCore/src/main/java/edu/vu/isis/ammo/core/network/INetChannel.java
+++ AmmoCore/src/main/java/edu/vu/isis/ammo/core/network/INetChannel.java
@@ -12,6 +12,7 @@ purpose whatsoever, and to have or authorize others to do so.
 package edu.vu.isis.ammo.core.network;
 
 import android.content.Context;
+import edu.vu.isis.ammo.core.R;
 import edu.vu.isis.ammo.core.distributor.DistributorDataStore.DisposalState;
 
 /**
@@ -22,36 +23,48 @@ import edu.vu.isis.ammo.core.distributor.DistributorDataStore.DisposalState;
 public interface INetChannel {
     
     public enum State {
-        PENDING, // the work is pending
-        EXCEPTION, // the run failed by some unhandled exception
+        PENDING(R.color.status_pending, R.string.status_pending), // the work is pending
+        EXCEPTION(R.color.status_exception, R.string.status_exception), // the run failed by some unhandled exception
     
-        CONNECTING, // trying to connect
-        CONNECTED, // the socket is good an active
-        BUSY, // the socket is busy and no new work should be queued
-        READY, // the socket can now take additional requests
+        CONNECTING(R.color.status_connecting, R.string.status_connecting), // trying to connect
+        CONNECTED(R.color.status_transmitting, R.string.status_transmitting), // the socket is good an active
+        BUSY(R.color.status_busy, R.string.status_busy), // the socket is busy and no new work should be queued
+        READY(R.color.status_ready, R.string.status_ready), // the socket can now take additional requests
     
-        DISCONNECTED, // the socket is disconnected
-        STALE, // indicating there is a message
-        LINK_WAIT, // indicating the underlying link is down
-        LINK_ACTIVE, // indicating the underlying link is down -- unused
-        DISABLED, // indicating the link is disabled
+        DISCONNECTED(R.color.status_disconnected, R.string.status_disconnected), // the socket is disconnected
+        STALE(R.color.status_stale, R.string.status_stale), // indicating there is a message
+        LINK_WAIT(R.color.status_link_wait, R.string.status_link_wait), // indicating the underlying link is down
+        LINK_ACTIVE(R.color.status_link_active, R.string.status_link_active), // indicating the underlying link is down -- unused
+        DISABLED(R.color.status_disabled, R.string.status_disabled), // indicating the link is disabled
     
-        WAIT_CONNECT, // waiting for connection
-        SENDING, // indicating the next thing is the size
-        TAKING, // indicating the next thing is the size
-        INTERRUPTED, // the run was canceled via an interrupt
+        WAIT_CONNECT(R.color.status_waiting_conn, R.string.status_waiting), // waiting for connection
+        SENDING(R.color.status_sending, R.string.status_sending), // indicating the next thing is the size
+        TAKING(R.color.status_taking, R.string.status_taking), // indicating the next thing is the size
+        INTERRUPTED(R.color.status_interrupted, R.string.status_interrupted), // the run was canceled via an interrupt
     
-        SHUTDOWN, // the run is being stopped -- unused
-        START, // indicating the next thing is the size
-        RESTART, // indicating the next thing is the size
-        WAIT_RECONNECT, // waiting for connection
-        STARTED, // indicating there is a message
-        SIZED, // indicating the next thing is a checksum
-        CHECKED, // indicating the bytes are being read
-        DELIVER, // indicating the message has been read
+        SHUTDOWN(R.color.status_shutdown, R.string.status_shutdown), // the run is being stopped -- unused
+        START(R.color.status_start, R.string.status_start), // indicating the next thing is the size
+        RESTART(R.color.status_start, R.string.status_start), // indicating the next thing is the size
+        WAIT_RECONNECT(R.color.status_waiting_conn, R.string.status_waiting), // waiting for connection
+        STARTED(R.color.status_started, R.string.status_started), // indicating there is a message
+        SIZED(R.color.status_sized, R.string.status_sized), // indicating the next thing is a checksum
+        CHECKED(R.color.status_checked, R.string.status_checked), // indicating the bytes are being read
+        DELIVER(R.color.status_deliver, R.string.status_deliver); // indicating the message has been read
+        
+        private final int mColorRes;
+        private final int mTextRes;
         
         State(int colorRes, int textRes) {
-            
+            mColorRes = colorRes;
+            mTextRes = textRes;
+        }
+        
+        public int getColorRes() {
+            return mColorRes;
+        }
+        
+        public int getTextRes() {
+            return mTextRes;
         }
     }
 
