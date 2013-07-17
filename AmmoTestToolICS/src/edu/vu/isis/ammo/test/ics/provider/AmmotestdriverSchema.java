b313c22c1a9bdff474d0e1c9c278d52695ec582f
Thomas <thomas.c.adams@vanderbilt.edu>
2013-07-17 15:18:34 -0500

    Fixed the ICS test tool

---
+++ AmmoTestToolICS/src/edu/vu/isis/ammo/test/ics/provider/AmmotestdriverSchema.java
@@ -0,0 +1,25 @@
+// THIS IS GENERATED CODE, WHEN YOU COMPLETE YOUR HAND EDITS REMOVE THIS LINE
+package edu.vu.isis.ammo.test.ics.provider;
+
+public class AmmotestdriverSchema extends AmmotestdriverSchemaBase {
+   public final static String VERSION = "1.7.0";
+   public static final int DATABASE_VERSION = 1;
+
+      public static class BaselineTableSchema extends BaselineTableSchemaBase {
+        public final static String VERSION = "1.7.0";
+         protected BaselineTableSchema() { super(); }
+      /**
+        Add relation constants as appropriate.
+        i.e.
+        public static final String <NAME> = "<sponsor>.provider.<name>.<table>.action.<NAME>";
+        e.g.
+        public static final String CONSTANT = "edu.vu.isis.ammo.test.ics.provider.ammotestdriver.baseline.action.CONSTANT";
+
+        public static final String PRIORITY_SORT_ORDER = 
+               BaselineTableSchemaBase.EXPIRATION + " DESC, " +
+               BaselineTableSchemaBase.MODIFIED_DATE + " DESC ";
+      */
+      }
+
+
+}
