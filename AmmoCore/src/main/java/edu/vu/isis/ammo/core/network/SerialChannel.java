7e597f65c600a55d60413c0b77ee3f8b9a7ba11b
Mike James <mcj@isis.vanderbilt.edu>
2013-07-31 17:44:47 -0500

    Added "ttyACM*" devices to the list of TTYs to test.

--- AmmoCore/src/main/java/edu/vu/isis/ammo/core/network/SerialChannel.java
+++ AmmoCore/src/main/java/edu/vu/isis/ammo/core/network/SerialChannel.java
@@ -515,7 +515,17 @@ public class SerialChannel extends NetChannel
                                "/dev/ttyUSB6",
                                "/dev/ttyUSB7",
                                "/dev/ttyUSB8",
-                               "/dev/ttyUSB9" };
+                               "/dev/ttyUSB9",
+                               "/dev/ttyACM0",
+                               "/dev/ttyACM1",
+                               "/dev/ttyACM2",
+                               "/dev/ttyACM3",
+                               "/dev/ttyACM4",
+                               "/dev/ttyACM5",
+                               "/dev/ttyACM6",
+                               "/dev/ttyACM7",
+                               "/dev/ttyACM8",
+                               "/dev/ttyACM9" };
             
             List<String> result = new ArrayList<String>();
             for ( String n : names ) {
